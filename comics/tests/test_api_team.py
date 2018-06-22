a15057b31c824c9b97094a211eab63572f2268e6
Brian Pepple <bdpepple@gmail.com>
2018-06-22 14:57:09 -0400

    Refactor the teams tests to use token auth.

--- comics/tests/test_api_team.py
+++ comics/tests/test_api_team.py
@@ -1,62 +1,66 @@
+import json
+
 from django.contrib.auth.models import User
-from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient, APITestCase
 
 from comics.models import Team
 from comics.serializers import TeamSerializer
 
 
-class TestCaseBase(TestCase):
-
-    def _create_user(self):
-        user = User.objects.create(username='brian')
-        user.set_password('1234')
-        user.save()
-
-        return user
-
-    def _client_login(self):
-        self.client.login(username='brian', password='1234')
-
-
-class GetAllTeamsTest(TestCaseBase):
+class GetAllTeamsTest(APITestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls._create_user(cls)
-
         Team.objects.create(cvid='1234', cvurl='http://1.com',
                             name='Teen Titans', slug='teen-titans')
         Team.objects.create(cvid='4321', cvurl='http://2.com',
                             name='The Avengers', slug='the-avengers')
 
     def setUp(self):
-        self._client_login()
+        self.email = 'brian@test.com'
+        self.username = 'brian'
+        self.password = 'test!thwip'
+        self.user = User.objects.create_user(
+            self.username, self.email, self.password)
+
+        self.client = APIClient()
+        self.token = Token.objects.get(user__username=self.username)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
 
     def test_view_url_accessible_by_name(self):
         resp = self.client.get(reverse('api:team-list'))
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertTrue(len(json.loads(resp.content)) == Team.objects.count())
 
     def test_unauthorized_view_url(self):
-        self.client.logout()
+        # Clear the credentials.
+        self.client.credentials()
         resp = self.client.get(reverse('api:team-list'))
-        self.assertEqual(resp.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(resp.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
-class GetSingleTeamTest(TestCaseBase):
+class GetSingleTeamTest(APITestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls._create_user(cls)
-
         cls.titans = Team.objects.create(
             cvid='1234', cvurl='http://1.com', name='Teen Titans', slug='teen-titans')
         cls.avengers = Team.objects.create(
             cvid='4321', cvurl='http://2.com', name='The Avengers', slug='the-avengers')
 
     def setUp(self):
-        self._client_login()
+        self.email = 'tom@test.com'
+        self.username = 'tom'
+        self.password = 'test!thwip'
+        self.user = User.objects.create_user(
+            self.username, self.email, self.password)
+
+        self.client = APIClient()
+        self.token = Token.objects.get(user__username=self.username)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
 
     def test_get_valid_single_team(self):
         response = self.client.get(
@@ -72,7 +76,8 @@ class GetSingleTeamTest(TestCaseBase):
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_unauthorized_view_url(self):
-        self.client.logout()
+        # Clear the credentials.
+        self.client.credentials()
         response = self.client.get(
             reverse('api:team-detail', kwargs={'slug': self.avengers.slug}))
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
