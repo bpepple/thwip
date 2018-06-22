94c300c6bb8293e45f21e441cd6dfcd355c73766
Brian Pepple <bdpepple@gmail.com>
2018-06-22 14:53:17 -0400

    Refactor thep series tests to use token auth.

--- comics/tests/test_api_series.py
+++ comics/tests/test_api_series.py
@@ -1,33 +1,21 @@
+import json
+
 from django.contrib.auth.models import User
-from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
+from rest_framework.authtoken.models import Token
 from rest_framework.request import Request
+from rest_framework.test import APIClient, APITestCase
 from rest_framework.test import APIRequestFactory
 
 from comics.models import Series, Publisher
 from comics.serializers import SeriesSerializer
 
 
-class TestCaseBase(TestCase):
-
-    def _create_user(self):
-        user = User.objects.create(username='brian')
-        user.set_password('1234')
-        user.save()
-
-        return user
-
-    def _client_login(self):
-        self.client.login(username='brian', password='1234')
-
-
-class GetAllSeriesTest(TestCaseBase):
+class GetAllSeriesTest(APITestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls._create_user(cls)
-
         publisher_obj = Publisher.objects.create(
             name='DC Comics', slug='dc-comics')
         Series.objects.create(cvid='1234', cvurl='http://1.com',
@@ -36,24 +24,33 @@ class GetAllSeriesTest(TestCaseBase):
                               name='Batman', slug='batman', publisher=publisher_obj)
 
     def setUp(self):
-        self._client_login()
+        self.email = 'brian@test.com'
+        self.username = 'brian'
+        self.password = 'test!thwip'
+        self.user = User.objects.create_user(
+            self.username, self.email, self.password)
+
+        self.client = APIClient()
+        self.token = Token.objects.get(user__username=self.username)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
 
     def test_view_url_accessible_by_name(self):
         resp = self.client.get(reverse('api:series-list'))
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertTrue(len(json.loads(resp.content))
+                        == Series.objects.count())
 
     def test_unauthorized_view_url(self):
-        self.client.logout()
+        # Clear the credentials.
+        self.client.credentials()
         resp = self.client.get(reverse('api:series-list'))
-        self.assertEqual(resp.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(resp.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
-class GetSingleSeriesTest(TestCaseBase):
+class GetSingleSeriesTest(APITestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls._create_user(cls)
-
         factory = APIRequestFactory()
         request = factory.get('/')
 
@@ -67,9 +64,17 @@ class GetSingleSeriesTest(TestCaseBase):
                                          publisher=publisher_obj)
 
     def setUp(self):
-        self._client_login()
+        self.email = 'tom@test.com'
+        self.username = 'tom'
+        self.password = 'test!thwip'
+        self.user = User.objects.create_user(
+            self.username, self.email, self.password)
 
-    def test_get_valid_single_issue(self):
+        self.client = APIClient()
+        self.token = Token.objects.get(user__username=self.username)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+    def test_get_valid_single_series(self):
         resp = self.client.get(
             reverse('api:series-detail', kwargs={'slug': self.thor.slug}))
         series = Series.objects.get(slug=self.thor.slug)
@@ -77,8 +82,14 @@ class GetSingleSeriesTest(TestCaseBase):
         self.assertEqual(resp.data, serializer.data)
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
 
+    def test_get_invalid_single_series(self):
+        response = self.client.get(
+            reverse('api:arc-detail', kwargs={'slug': 'batman'}))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
     def test_unauthorized_view_url(self):
-        self.client.logout()
+        # Clear the credentials.
+        self.client.credentials()
         response = self.client.get(
             reverse('api:series-detail', kwargs={'slug': self.thor.slug}))
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
