7fe0e114f285af021a6414f95b6b15c740680fd4
Brian Pepple <bdpepple@gmail.com>
2018-06-22 14:42:17 -0400

    Refactor the publisher tests to use token auth.

--- comics/tests/test_api_publisher.py
+++ comics/tests/test_api_publisher.py
@@ -1,58 +1,63 @@
+import json
+
 from django.contrib.auth.models import User
-from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient, APITestCase
 
 from comics.models import Publisher
 from comics.serializers import PublisherSerializer
 
 
-class TestCaseBase(TestCase):
-
-    def _create_user(self):
-        user = User.objects.create(username='brian')
-        user.set_password('1234')
-        user.save()
-
-        return user
-
-    def _client_login(self):
-        self.client.login(username='brian', password='1234')
-
-
-class GetAllPublisherTest(TestCaseBase):
+class GetAllPublisherTest(APITestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls._create_user(cls)
-
         Publisher.objects.create(name='DC Comics', slug='dc-comics')
         Publisher.objects.create(name='Marvel', slug='marvel')
 
     def setUp(self):
-        self._client_login()
+        self.email = 'brian@test.com'
+        self.username = 'brian'
+        self.password = 'test!thwip'
+        self.user = User.objects.create_user(
+            self.username, self.email, self.password)
+
+        self.client = APIClient()
+        self.token = Token.objects.get(user__username=self.username)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
 
     def test_view_url_accessible_by_name(self):
         resp = self.client.get(reverse('api:publisher-list'))
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertTrue(len(json.loads(resp.content))
+                        == Publisher.objects.count())
 
     def test_unauthorized_view_url(self):
-        self.client.logout()
+        # Clear the credentials.
+        self.client.credentials()
         resp = self.client.get(reverse('api:publisher-list'))
-        self.assertEqual(resp.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(resp.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
-class GetSinglePublisherTest(TestCaseBase):
+class GetSinglePublisherTest(APITestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls._create_user(cls)
-
         cls.dc = Publisher.objects.create(name='DC Comics', slug='dc-comics')
         cls.marvel = Publisher.objects.create(name='Marvel', slug='marvel')
 
     def setUp(self):
-        self._client_login()
+        self.email = 'tom@test.com'
+        self.username = 'tom'
+        self.password = 'test!thwip'
+        self.user = User.objects.create_user(
+            self.username, self.email, self.password)
+
+        self.client = APIClient()
+        self.token = Token.objects.get(user__username=self.username)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
 
     def test_get_valid_single_publisher(self):
         response = self.client.get(
@@ -68,7 +73,8 @@ class GetSinglePublisherTest(TestCaseBase):
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_unauthorized_view_url(self):
-        self.client.logout()
+        # Clear the credentials.
+        self.client.credentials()
         response = self.client.get(
             reverse('api:publisher-detail', kwargs={'slug': self.dc.slug}))
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
