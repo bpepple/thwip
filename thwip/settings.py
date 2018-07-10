bc92022d6d98b213101fba7ef618156f03836d1a
Brian Pepple <bdpepple@gmail.com>
2018-07-09 22:25:32 -0400

    Add corsheaders to the project for the frontend.

--- thwip/settings.py
+++ thwip/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     'solo.apps.SoloAppConfig',
     'comics',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -86,6 +88,12 @@ DATABASES = {
         'PORT': '',
     }
 }
+
+# Cors
+CORS_ORIGIN_WHITELIST = (
+    'localhost:3000',
+    '127.0.0.1:3000'
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
