ec2fd526b910db76bb35c023ad44e4e3a92fd26b
Brian Pepple <bdpepple@gmail.com>
2018-07-12 18:27:30 -0400

    Add the django-debug-toolbar to the project.

--- thwip/urls.py
+++ thwip/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('comics.urls')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
