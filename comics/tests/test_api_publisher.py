f86181288c9928b1eb634f4f28f2000e53cccaff
Brian Pepple <bdpepple@gmail.com>
2018-07-16 10:43:15 -0400

    Fix the publisher api tests.

--- comics/tests/test_api_publisher.py
+++ comics/tests/test_api_publisher.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
@@ -7,52 +6,24 @@ from comics.models import Publisher
 from comics.serializers import PublisherSerializer
 
 
-class TestCaseBase(TestCase):
-
-    def _create_user(self):
-        user = User.objects.create(username='brian')
-        user.set_password('1234')
-        user.save()
-
-        return user
-
-    def _client_login(self):
-        self.client.login(username='brian', password='1234')
-
-
-class GetAllPublisherTest(TestCaseBase):
+class GetAllPublisherTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls._create_user(cls)
-
         Publisher.objects.create(name='DC Comics', slug='dc-comics')
         Publisher.objects.create(name='Marvel', slug='marvel')
-
-    def setUp(self):
-        self._client_login()
 
     def test_view_url_accessible_by_name(self):
         resp = self.client.get(reverse('api:publisher-list'))
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
 
-    def test_unauthorized_view_url(self):
-        self.client.logout()
-        resp = self.client.get(reverse('api:publisher-list'))
-        self.assertEqual(resp.status_code, status.HTTP_403_FORBIDDEN)
 
-
-class GetSinglePublisherTest(TestCaseBase):
+class GetSinglePublisherTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls._create_user(cls)
-
         cls.dc = Publisher.objects.create(name='DC Comics', slug='dc-comics')
         cls.marvel = Publisher.objects.create(name='Marvel', slug='marvel')
-
-    def setUp(self):
-        self._client_login()
 
     def test_get_valid_single_publisher(self):
         response = self.client.get(
@@ -66,9 +37,3 @@ class GetSinglePublisherTest(TestCaseBase):
         response = self.client.get(
             reverse('api:publisher-detail', kwargs={'slug': 'dark-horse'}))
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-
-    def test_unauthorized_view_url(self):
-        self.client.logout()
-        response = self.client.get(
-            reverse('api:publisher-detail', kwargs={'slug': self.dc.slug}))
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
