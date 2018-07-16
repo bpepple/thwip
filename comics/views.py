c419d2a270ed814ce3b7c934679bb76d6ef949ff
Brian Pepple <bdpepple@gmail.com>
2018-07-16 10:14:04 -0400

    Fix the Views imports sort.

--- comics/views.py
+++ comics/views.py
@@ -1,8 +1,7 @@
+from rest_framework import mixins
 from rest_framework import viewsets
 from rest_framework.decorators import detail_route
 from rest_framework.response import Response
-
-from rest_framework import mixins
 
 from comics.models import (Arc, Character, Creator,
                            Issue, Publisher, Series,
