b3fbf4d625d1256d36fe933c1fdbc629a3841fd4
Brian Pepple <bdpepple@gmail.com>
2018-07-16 10:39:16 -0400

    Fix the issues api tests.

--- comics/tests/test_api_issues.py
+++ comics/tests/test_api_issues.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
@@ -14,25 +13,10 @@ issue_date = timezone.now().date()
 mod_time = timezone.now()
 
 
-class TestCaseBase(TestCase):
-
-    def _create_user(self):
-        user = User.objects.create(username='brian')
-        user.set_password('1234')
-        user.save()
-
-        return user
-
-    def _client_login(self):
-        self.client.login(username='brian', password='1234')
-
-
-class GetAllIssueTest(TestCaseBase):
+class GetAllIssueTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls._create_user(cls)
-
         publisher_obj = Publisher.objects.create(
             name='DC Comics', slug='dc-comics')
         series_obj = Series.objects.create(
@@ -42,25 +26,15 @@ class GetAllIssueTest(TestCaseBase):
         Issue.objects.create(cvid='4321', cvurl='http://2.com', slug='batman-1',
                              file='/home/b.cbz', mod_ts=mod_time, date=issue_date, number='1', series=series_obj)
 
-    def setUp(self):
-        self._client_login()
-
     def test_view_url_accessible_by_name(self):
         resp = self.client.get(reverse('api:issue-list'))
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
 
-    def test_unauthorized_view_url(self):
-        self.client.logout()
-        resp = self.client.get(reverse('api:issue-list'))
-        self.assertEqual(resp.status_code, status.HTTP_403_FORBIDDEN)
 
-
-class GetSingleIssueTest(TestCaseBase):
+class GetSingleIssueTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls._create_user(cls)
-
         factory = APIRequestFactory()
         request = factory.get('/')
 
@@ -75,9 +49,6 @@ class GetSingleIssueTest(TestCaseBase):
         cls.superman = Issue.objects.create(cvid='1234', cvurl='http://1.com', slug='superman-1',
                                             file='/home/a.cbz', mod_ts=mod_time, date=issue_date, number='1', series=series_obj)
 
-    def setUp(self):
-        self._client_login()
-
     def test_get_valid_single_issue(self):
         response = self.client.get(
             reverse('api:issue-detail', kwargs={'slug': self.superman.slug}))
@@ -85,9 +56,3 @@ class GetSingleIssueTest(TestCaseBase):
         serializer = IssueSerializer(issue, context=self.serializer_context)
         self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_unauthorized_view_url(self):
-        self.client.logout()
-        response = self.client.get(
-            reverse('api:issue-detail', kwargs={'slug': self.superman.slug}))
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
