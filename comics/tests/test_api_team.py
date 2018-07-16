324c499ab8be04da921b039818d0bcf5757b0371
Brian Pepple <bdpepple@gmail.com>
2018-07-16 10:49:32 -0400

    Fix the team api tests.

--- comics/tests/test_api_team.py
+++ comics/tests/test_api_team.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
@@ -7,56 +6,28 @@ from comics.models import Team
 from comics.serializers import TeamSerializer
 
 
-class TestCaseBase(TestCase):
-
-    def _create_user(self):
-        user = User.objects.create(username='brian')
-        user.set_password('1234')
-        user.save()
-
-        return user
-
-    def _client_login(self):
-        self.client.login(username='brian', password='1234')
-
-
-class GetAllTeamsTest(TestCaseBase):
+class GetAllTeamsTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls._create_user(cls)
-
         Team.objects.create(cvid='1234', cvurl='http://1.com',
                             name='Teen Titans', slug='teen-titans')
         Team.objects.create(cvid='4321', cvurl='http://2.com',
                             name='The Avengers', slug='the-avengers')
 
-    def setUp(self):
-        self._client_login()
-
     def test_view_url_accessible_by_name(self):
         resp = self.client.get(reverse('api:team-list'))
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
 
-    def test_unauthorized_view_url(self):
-        self.client.logout()
-        resp = self.client.get(reverse('api:team-list'))
-        self.assertEqual(resp.status_code, status.HTTP_403_FORBIDDEN)
 
-
-class GetSingleTeamTest(TestCaseBase):
+class GetSingleTeamTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls._create_user(cls)
-
         cls.titans = Team.objects.create(
             cvid='1234', cvurl='http://1.com', name='Teen Titans', slug='teen-titans')
         cls.avengers = Team.objects.create(
             cvid='4321', cvurl='http://2.com', name='The Avengers', slug='the-avengers')
-
-    def setUp(self):
-        self._client_login()
 
     def test_get_valid_single_team(self):
         response = self.client.get(
@@ -70,9 +41,3 @@ class GetSingleTeamTest(TestCaseBase):
         response = self.client.get(
             reverse('api:team-detail', kwargs={'slug': 'justice-league'}))
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-
-    def test_unauthorized_view_url(self):
-        self.client.logout()
-        response = self.client.get(
-            reverse('api:team-detail', kwargs={'slug': self.avengers.slug}))
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
