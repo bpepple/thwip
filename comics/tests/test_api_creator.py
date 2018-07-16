3699871a50f618da4acdd243e751b36592905f05
Brian Pepple <bdpepple@gmail.com>
2018-07-16 10:37:04 -0400

    Fix the creator api tests.

--- comics/tests/test_api_creator.py
+++ comics/tests/test_api_creator.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
@@ -7,56 +6,28 @@ from comics.models import Creator
 from comics.serializers import CreatorSerializer
 
 
-class TestCaseBase(TestCase):
-
-    def _create_user(self):
-        user = User.objects.create(username='brian')
-        user.set_password('1234')
-        user.save()
-
-        return user
-
-    def _client_login(self):
-        self.client.login(username='brian', password='1234')
-
-
-class GetAllCreatorsTest(TestCaseBase):
+class GetAllCreatorsTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls._create_user(cls)
-
         Creator.objects.create(
             cvid='1234', cvurl='http://1.com', name='John Byrne', slug='john-byrne')
         Creator.objects.create(cvid='4321', cvurl='http://2.com',
                                name='Walter Simonson', slug='walter-simonson')
 
-    def setUp(self):
-        self._client_login()
-
     def test_view_url_accessible_by_name(self):
         resp = self.client.get(reverse('api:creator-list'))
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
 
-    def test_unauthorized_view_url(self):
-        self.client.logout()
-        resp = self.client.get(reverse('api:creator-list'))
-        self.assertEqual(resp.status_code, status.HTTP_403_FORBIDDEN)
 
-
-class GetSingleCreatorTest(TestCaseBase):
+class GetSingleCreatorTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls._create_user(cls)
-
         cls.john = Creator.objects.create(
             cvid='1234', cvurl='http://1.com', name='John Byrne', slug='john-byrne')
         cls.walter = Creator.objects.create(
             cvid='4321', cvurl='http://2.com', name='Walter Simonson', slug='walter-simonson')
-
-    def setUp(self):
-        self._client_login()
 
     def test_get_valid_single_creator(self):
         response = self.client.get(
@@ -70,9 +41,3 @@ class GetSingleCreatorTest(TestCaseBase):
         response = self.client.get(
             reverse('api:creator-detail', kwargs={'slug': 'art-adams'}))
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-
-    def test_unauthorized_view_url(self):
-        self.client.logout()
-        response = self.client.get(
-            reverse('api:creator-detail', kwargs={'slug': self.walter.slug}))
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
