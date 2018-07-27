01068f1a7edb3005f64e5035c5e78ef5d74fca98
Brian Pepple <bdpepple@gmail.com>
2018-07-27 14:53:58 -0400

    Add unread_issue_count to series fields.

--- comics/serializers.py
+++ comics/serializers.py
@@ -65,12 +65,14 @@ class SeriesSerializer(serializers.HyperlinkedModelSerializer):
     publisher = serializers.SlugRelatedField(many=False, read_only=True,
                                              slug_field='slug')
     issue_count = serializers.ReadOnlyField
+    unread_issue_count = serializers.ReadOnlyField
     image = SeriesImageSerializer(source='issue_set.first', many=False)
 
     class Meta:
         model = Series
         fields = ('slug', 'cvurl', 'name', 'sort_title', 'publisher',
-                  'year', 'desc', 'issue_count', 'image')
+                  'year', 'desc', 'issue_count', 'unread_issue_count',
+                  'image')
         lookup_field = 'slug'
 
     def to_representation(self, obj):
