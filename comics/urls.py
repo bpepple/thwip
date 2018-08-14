68d9bbfa5bb47a3060a667d62c923c78de8a3183
Brian Pepple <bdpepple@gmail.com>
2018-08-14 14:08:09 -0400

    Enable drf jwt authentication.

--- comics/urls.py
+++ comics/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_jwt.views import obtain_jwt_token
 
 from comics.views import (IssueViewSet, PublisherViewSet, SeriesViewSet)
 
@@ -14,6 +15,7 @@ router.register('series', SeriesViewSet)
 app_name = 'api'
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('api-token-auth/', obtain_jwt_token)
 ]
 
 if settings.DEBUG:
--- thwip/settings.py
+++ thwip/settings.py
@@ -90,6 +90,14 @@ DATABASES = {
 }
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 30
 }
