9a0685152cb43c1c0c5f25a073d5477308546005
Brian Pepple <bdpepple@gmail.com>
2018-08-27 14:14:43 -0400

    Add some more issue string tests.

--- comics/tests/test_comicapi_issue_string.py
+++ comics/tests/test_comicapi_issue_string.py
@@ -13,6 +13,18 @@ class TestIssueString(SimpleTestCase):
         val = IssueString('1½').asFloat()
         self.assertEqual(val, 1.5)
 
+    def test_issue_float_half(self):
+        val = IssueString('½').asFloat()
+        self.assertEqual(val, 0.5)
+
+    def test_issue_verify_float(self):
+        val = IssueString('1.5').asFloat()
+        self.assertEqual(val, 1.5)
+
+    def test_issue_string_no_value_as_int(self):
+        val = IssueString('').asInt()
+        self.assertIsNone(val)
+
     def test_issue_int(self):
         val = IssueString('1').asInt()
         self.assertEqual(val, 1)
@@ -28,3 +40,7 @@ class TestIssueString(SimpleTestCase):
     def test_issue_string_minus_one(self):
         val = IssueString('-1').asString(3)
         self.assertEqual(val, '-001')
+
+    def test_issue_string_none_value(self):
+        val = IssueString('Test').asString()
+        self.assertEqual(val, 'Test')
