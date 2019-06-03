a0560c6aca4349b09c3882e2ecfa6db6af31aa13
Brian Pepple <bdpepple@gmail.com>
2019-06-03 15:40:16 -0400

    Add scheme for CORS_ORIGIN_WHITELIST
    * This is needed for django-cors-headers >= 3.0.0

--- thwip/settings.py
+++ thwip/settings.py
@@ -104,8 +104,8 @@ REST_FRAMEWORK = {
 
 # Cors
 CORS_ORIGIN_WHITELIST = (
-    'localhost:3000',
-    '127.0.0.1:3000'
+    'http://localhost:3000',
+    'http://127.0.0.1:3000'
 )
 
 CORS_ALLOW_CREDENTIALS = True
